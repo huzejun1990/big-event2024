a2174d147acdd689804841d6562702350ca99619
huzejun <961639130@qq.com>
2024-10-06 20:26:28 +0800

    后端功能开发 文件上传+阿里云oss 2024-10-6 20:26:17

--- src/main/java/com/dream/controller/FileUploadController.java
+++ src/main/java/com/dream/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.dream.controller;
 
 import com.dream.pojo.Result;
+import com.dream.utils.AliOssUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,13 +18,16 @@ import java.util.UUID;
 public class FileUploadController {
 
     @PostMapping("/upload")
-    public Result<String> upload(MultipartFile file) throws IOException {
+    public Result<String> upload(MultipartFile file) throws Exception {
         //把文件的内容存储到本地磁盘上
         String originalFilename = file.getOriginalFilename();
         //保证文件的名字是唯一的，从而保证防止文件覆盖
         String fileName = UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf("."));
-        file.transferTo(new File("E:\\files\\"+fileName));
-        return Result.success("url访问地址...");
+        //本地存储
+        //        file.transferTo(new File("E:\\files\\"+fileName));
+        String url = AliOssUtil.uploadFile(fileName, file.getInputStream());
+//        return Result.success("url访问地址...");
+        return Result.success(url);
     }
 
 
